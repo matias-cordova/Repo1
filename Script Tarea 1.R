f73a4cba00d5d10d120ae302a171c0c45c0e5389
matias-cordova <m.cordova@socialxchange.org>
2021-05-21 10:15:23 -0400

    Gráfica de pétalos
    Se agrega grafica de pétalos

--- Script Tarea 1.R
+++ Script Tarea 1.R
@@ -1,3 +1,5 @@
 plot(x = iris$Species , main = "Gráfica de Flores",
      xlab = "Especies de Flores", ylab = "Frecuencia",
      col = c("royalblue", "seagreen", "yellow"))
+hist(iris$Sepal.Length, main = "Frecuencia del largo de los pétalos",
+     xlab = "Largo del pétalo (cm)", col = rainbow(8))
